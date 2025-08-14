9d58055ca3d5e2e11d5297473c18aedc4011025e
Lucas C <lacameron@DESKTOP-1TQ836V.localdomain>
2025-08-14 13:10:12 -0700

    starting to build window

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+#include <cmath>
+#include <SFML/Graphics.hpp>
+
+int main() { 
+    sf::RenderWindow window(sf::VideoMode(screenWidth,screenHeight). "SFML Raycaster");
+
+    sf::Vector2<double> playerPos(5.0, 5.0);
+    sf::Vector2<double> playerDir(1.0, 0.0);
+    sf::Vector2<double> playerPlane(0.0, 0.9); 
+    
+    while(window.isOpen()) {
+
+        sf::Event event{};
+        while(window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) window.close();
+        }
+
+        window.clear(sf::Color(30, 30, 30));
+
+
+}
